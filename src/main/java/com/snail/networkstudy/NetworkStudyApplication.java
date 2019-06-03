68325ba3bc70737e47ee5786d4dfeca8615d7d75
xuchuanliang <xuchuanliang361@163.com>
2019-06-03 08:20:29 +0800

    init

---
+++ src/main/java/com/snail/networkstudy/NetworkStudyApplication.java
@@ -0,0 +1,13 @@
+package com.snail.networkstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snail/networkstudy/NetworkStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snail.networkstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetworkStudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
